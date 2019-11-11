e06d08f0ba52b4354b97f42e09082b1b32d21e69
Can Ni <cni@daisy.ece.utexas.edu>
2019-11-11 10:00:03 -0600

    I am just commiting this to see what happens

--- src/normalize_ff.v
+++ src/normalize_ff.v
@@ -4,5 +4,18 @@ P_normalized
 );
 
 
+//------------------------Input Port-----------------------------//
+input	wire	[31:0]	P;
 
+
+//------------------------Output Port-----------------------------//
+output	wire	[31:0] 	P_normalized;
+
+//------------------------Internal Variables-----------------------//
+reg			S; //Sign bit
+reg		[ 7:0]	E; //Exponent
+reg		[23:0]  M; //Mantissa
+
+//------------------------Code Starts Here-------------------------//
+ 
 endmodule
